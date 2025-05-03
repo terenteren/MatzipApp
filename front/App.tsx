aa76f078392c134a0eca58384e091f38a254f9b6
jaewon jang <jaewonjang@jaewonui-MacBookPro-2.local>
2025-05-03 12:43:57 +0900

    Initial commit: MatzipApp React Native project

---
+++ front/App.tsx
@@ -0,0 +1,13 @@
+import {NavigationContainer} from '@react-navigation/native';
+import React from 'react';
+import RootNavigator from './src/navigations/root/RootNavigator';
+
+function App() {
+  return (
+    <NavigationContainer>
+      <RootNavigator />
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ front/src/screens/auth/AuthHomeScreen.tsx
@@ -0,0 +1,33 @@
+import {StackScreenProps} from '@react-navigation/stack';
+import React from 'react';
+import {SafeAreaView, Button, StyleSheet, View} from 'react-native';
+import {AuthStackParamList} from '../../navigations/stack/AuthStackNavigator';
+import {authNavigations} from '../../constants/navigations';
+import CustomButton from '../../components/CustomButton';
+
+type AuthHomeScreenProps = StackScreenProps<
+  AuthStackParamList,
+  typeof authNavigations.AUTH_HOME
+>;
+
+function AuthHomeScreen({navigation}: AuthHomeScreenProps) {
+  return (
+    <SafeAreaView>
+      <View>
+        <CustomButton
+          label="로그인하기"
+          onPress={() => navigation.navigate(authNavigations.LOGIN)}
+        />
+        <CustomButton
+          label="회원가입하기"
+          variant="outlined"
+          onPress={() => navigation.navigate(authNavigations.SIGNUP)}
+        />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default AuthHomeScreen;
