69c59c9c67ae56e945276fbfecd68a3358e75034
jaewon jang <jaewonjang@jaewonui-MacBookPro-2.local>
2025-05-03 12:51:48 +0900

    첫번째 커밋 메세지 작성용 1. 프로젝트 생성 2. 환경설정 3. 네비게이션 4. 공통 컴포넌트 구현

--- front/src/screens/feed/FeedHomeScreen.tsx
+++ front/src/screens/feed/FeedHomeScreen.tsx
@@ -4,7 +4,7 @@ import {StyleSheet, Text, View} from 'react-native';
 function FeedHomeScreen() {
   return (
     <View>
-      <Text>피드</Text>
+      <Text>피드 FeedHomeScreen</Text>
     </View>
   );
 }
